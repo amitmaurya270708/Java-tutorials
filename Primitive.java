6c3ad0f68d44ed547bab52044d0713e874f19663
Amit <amitkumarmaurya270708@gmail.com>
2026-06-12 21:07:36 +0530

    Day 6

---
+++ Primitive.java
@@ -0,0 +1,28 @@
+class Primitive{
+    public static void main(String []args){
+        int population = 1_000_000;
+        System.out.println("Int : Population : " + population );
+
+        byte age = 25;
+        System.out.println("Byte : age : " + age );
+
+        short temperature = -100;
+        System.out.println("Short : Temperature : " + temperature );
+
+        char grade = 'A';
+        System.out.println("Char : Grade : " + grade );
+
+        boolean isActive = false;
+        System.out.println("Boolean : isActive : " + isActive );
+
+        float price = 100.56f;
+        System.out.println("Float : Price : " + price );
+
+        float price2 = (float) 100.56;
+        System.out.println("Float : Price2 : " + price2 );
+
+        double price3 = 200.50;
+        System.out.println("Double : Price3  : " + price3 );
+
+    }
+}
